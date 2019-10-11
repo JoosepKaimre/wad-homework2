42a1e41dc691edacf759b519f5c1e68d10bd412a
magnuspaal <mmagnuspaal@gmail.com>
2019-10-11 20:09:35 +0300

    js file, profile and courses buttons work

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#profile-button').click(function () {
+        $('#profile-container').addClass('active')
+        $('#courses-container').removeClass('active')
+        $('#profile-button').addClass('active')
+        $('#courses-button').removeClass('active')
+    })
+
+    $('#courses-button').click(function () {
+        $('#profile-container').removeClass('active')
+        $('#courses-container').addClass('active')
+        $('#profile-button').removeClass('active')
+        $('#courses-button').addClass('active')
+    })
+});
